d32a12cbbde3a3d7b5535a3a508b2ca0814c087c
Mario Lazzari <mario.lazzari@gmail.com>
2022-01-05 09:53:39 +0100

    Done lesson 50

--- 04_modules/example/https.mjs
+++ 04_modules/example/https.mjs
@@ -1,6 +1,7 @@
 import { send } from "./request.js";
 import { read } from "./response.js";
-import { TIMEOUT } from "./request.js";
+// import { TIMEOUT } from "./request.js";
+import axios from "axios";
 
 function request(url, data) {
   send(url, data);
@@ -9,3 +10,15 @@ function request(url, data) {
 
 const res = request("http://www.google.com", "data");
 console.log(res);
+
+axios
+  .get("https://www.google.com")
+  .then(res => {
+    console.log(res);
+  })
+  .catch(err => {
+    console.log(err);
+  })
+  .then(() => {
+    console.log("Request finished");
+  });

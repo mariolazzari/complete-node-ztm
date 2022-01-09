2df372fdb4c1021c9f71397cb6e55bbd08adb6a2
Mario Lazzari <mario.lazzari@gmail.com>
2022-01-09 12:05:09 +0100

    Done lesson 203

---
+++ 14_auth/server.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const https = require("https");
+const path = require("path");
+const express = require("express");
+const helmet = require("helmet");
+
+const PORT = 3000;
+
+const app = express();
+app.use(helmet());
+
+app.get("/", (req, res) => {
+  const index = path.join(__dirname, "public", "index.html");
+  res.sendFile(index);
+});
+
+app.get("/secret", (req, res) => {
+  res.send("Your personale secret value is 42");
+});
+
+/*
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+*/
+
+// openssl req -x509 -newkey rsa:4096 -nodes -keyout key.pem -out cert.pem -days 365
+https
+  .createServer(
+    {
+      key: fs.readFileSync("key.pem"),
+      cert: fs.readFileSync("cert.pem"),
+    },
+    app
+  )
+  .listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  });

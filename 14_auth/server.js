5c75be42cd26f918fe5911e5c3947b3a0fbd07aa
Mario Lazzari <mario.lazzari@gmail.com>
2022-01-09 18:21:52 +0100

    Done lesson 213

--- 14_auth/server.js
+++ 14_auth/server.js
@@ -3,19 +3,69 @@ const https = require("https");
 const path = require("path");
 const express = require("express");
 const helmet = require("helmet");
+const passport = require("passport");
+const { Strategy } = require("passport-google-oauth20");
 
-const PORT = 3000;
+require("dotenv").config();
+const { PORT, CLIENT_ID, CLIENT_SECRET } = process.env;
+const AUTH_OPTIONS = {
+  clientID: CLIENT_ID,
+  clientSecret: CLIENT_SECRET,
+  callbackURL: "/auth/google/callback",
+};
+
+const verifyCallback = (accessToken, refreshToken, profile, done) => {
+  console.log("Google profile: ", profile);
+  done(null, profile);
+};
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
 
 const app = express();
 app.use(helmet());
+app.use(passport.initialize());
+
+// auth
+const checkUser = (req, res, next) => {
+  let isLogged = true;
+  if (!isLogged) {
+    return res.status(401).send("Unauthorized");
+  }
+  next();
+};
 
 app.get("/", (req, res) => {
   const index = path.join(__dirname, "public", "index.html");
   res.sendFile(index);
 });
 
-app.get("/secret", (req, res) => {
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["email"],
+  })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/failure",
+    successRedirect: "/",
+    session: true,
+  }),
+  (req, res) => {
+    console.log("Google called us back!");
+  }
+);
+
+app.get("/auth/logout", (req, res) => {});
+
+app.get("/secret", checkUser, (req, res) => {
   res.send("Your personale secret value is 42");
+});
+
+app.get("/failure", (req, res) => {
+  res.send("Login failed");
 });
 
 /*
@@ -25,14 +75,9 @@ app.listen(PORT, () => {
 */
 
 // openssl req -x509 -newkey rsa:4096 -nodes -keyout key.pem -out cert.pem -days 365
-https
-  .createServer(
-    {
-      key: fs.readFileSync("cert/key.pem"),
-      cert: fs.readFileSync("cert/cert.pem"),
-    },
-    app
-  )
-  .listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-  });
+const key = fs.readFileSync(path.join("cert", "key.pem"));
+const cert = fs.readFileSync(path.join("cert", "cert.pem"));
+
+https.createServer({ key, cert }, app).listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
